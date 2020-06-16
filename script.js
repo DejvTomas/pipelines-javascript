c765707f40239b76a96e9add280a818572ad8567
David Tomas <d.tomas@gmc.net>
2020-06-16 07:25:20 +0200

    add base of new structure

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+// get job id
+var Chalk = require("chalk");
+var jobId = process.argv[3];
+if (jobId === undefined) {
+    Chalk.red("JobId argument is missing - process exit");
+    process.exit();
+}
+console.log(process.argv[3]);
---
+++ script.ts
@@ -0,0 +1,10 @@
+
+// get job id
+import * as Chalk from 'chalk';
+
+let jobId = process.argv[3];
+if(jobId === undefined) {
+    Chalk.red("JobId argument is missing - process exit");
+    process.exit()
+}
+console.log(process.argv[3]);
